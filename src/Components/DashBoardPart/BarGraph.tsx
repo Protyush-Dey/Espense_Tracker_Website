677ec01d580113a04c2e56a0729a27eeb14edc9d
Protyush <mdey7054@gmail.com>
2026-04-12 02:33:00 +0530

    bar chart outline bug fix

--- src/Components/DashBoardPart/BarGraph.tsx
+++ src/Components/DashBoardPart/BarGraph.tsx
@@ -26,10 +26,9 @@ const BarGraph = () => {
   const activeMonth = "May";
 
   return (
-    <div className="w-full h-full bg" >
+    <div className="w-full h-full bg">
       <ResponsiveContainer width="100%" height="100%">
         <BarChart data={data} barCategoryGap="25%">
-          {/* X Axis */}
           <XAxis
             dataKey="name"
             axisLine={false}
@@ -37,13 +36,13 @@ const BarGraph = () => {
             tick={{ fill: "#6B7280", fontSize: 12 }}
           />
 
-          {/* Tooltip */}
           <Tooltip
             contentStyle={{
               borderRadius: "5px",
               fontWeight: "600",
               border: "none",
-             boxShadow: "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
+              boxShadow:
+                "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
               backgroundColor: "#EEEEEE",
             }}
             formatter={(value) => {
@@ -56,17 +55,17 @@ const BarGraph = () => {
             }}
             cursor={{ fill: "transparent" }}
           />
-          {/* Bars */}
-          <Bar dataKey="value" radius={[3, 3, 3, 3]} minPointSize={3} >
+
+          <Bar dataKey="value" radius={[3, 3, 3, 3]} minPointSize={3}>
             {data.map((entry) => (
               <Cell
                 key={entry.name}
                 fill={
                   entry.value === 0
-                    ? "#E5E7EB" // empty months (gray)
+                    ? "#E5E7EB"
                     : entry.name === activeMonth
-                      ? "#3B7AE8" // active month (blue)
-                      : "#E8EDF5" // normal
+                    ? "#3B7AE8"
+                    : "#E8EDF5"
                 }
                 cursor="pointer"
               />
