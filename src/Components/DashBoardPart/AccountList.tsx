ff99b9574e3f490fce8f52d30611b2621c761c26
Protyush <mdey7054@gmail.com>
2026-04-26 02:27:10 +0530

    Update AccountList.tsx

--- src/Components/DashBoardPart/AccountList.tsx
+++ src/Components/DashBoardPart/AccountList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import {useRef} from "react";
 import AccountCard from "./AccountCard";
 import leftIcon from "../../assets/Images/icon/horizontalScBtn.png";
 import { useQuery } from "@tanstack/react-query";
