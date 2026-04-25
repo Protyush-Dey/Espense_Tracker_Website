d0413d9c773b54a4479854c5cf64bc1fcf6bdf04
Protyush <mdey7054@gmail.com>
2026-04-25 14:52:20 +0530

    login and signup api call and validation and forget pass

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,8 +1,13 @@
 import api from "./api.ts";
 
-import type { LoginPayload } from "../types/authType.ts";
+import type { LoginPayload, SignUpPayload } from "../types/authType.ts";
 
 // login 
 export const login = (Data:LoginPayload) =>{
     return api.post("expTrack/user/logIn" , {json:Data}).json();
+}
+
+// signup
+export const signup = (Data:SignUpPayload) =>{
+    return api.post("expTrack/user/register" , {json:Data}).json();
 }
